976c56bfa3c3370df77edac025600d5808069fb5
Brandon Kim <bkim2490@gmail.com>
2019-02-06 12:14:26 +0900

    add music

--- src/data/albums.js
+++ src/data/albums.js
@@ -24,5 +24,49 @@ export default [{
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
+  }, {
+    title: 'Run the Jewels 2',
+    artist: 'Run the Jewels',
+    releaseInfo: '2014 Mass Appeal',
+    albumCover: '/assets/images/album_covers/rtj2.png',
+    slug: 'run-the-jewels-2',
+    songs: [
+      { title: 'Jeopardy', duration: '202', audioSrc: '/assets/music/RTJ2/01%20Jeopardy.mp3' },
+      { title: 'Oh My Darling Don\'t Cry', duration: '205', audioSrc: '/assets/music/RTJ2/02%20Oh%20My%20Darling%20Don\'t%20Cry.mp3' },
+      { title: 'Blockbuster Night Part 1', duration: '152', audioSrc: '/assets/music/RTJ2/03%20Blockbuster%20Night%20Part%201.mp3' },
+      { title: 'Close Your Eyes \(And Count To F***\) \(Feat. Zack De La Rocha\)', duration: '234', audioSrc: '/assets/music/RTJ2/04%20Close%20Your%20Eyes%20\(And%20Count%20To%20Fuck\)%20\(Feat.%20Zack%20De%20La%20Rocha\).mp3' },
+      { title: 'All My Life', duration: '188', audioSrc: '/assets/music/RTJ2/05%20All%20My%20Life.mp3' },
+      { title: 'Lie, Cheat, Steal', duration: '209', audioSrc: '/assets/music/RTJ2/06%20Lie,%20Cheat,%20Steal.mp3' },
+      { title: 'Early \(feat. BOOTS\)', duration: '224', audioSrc: '/assets/music/RTJ2/07%20Early%20\(feat.%20BOOTS\).mp3' },
+      { title: 'All Due Respect', duration: '168', audioSrc: '/assets/music/RTJ2/08%20All%20Due%20Respect%20\(feat.%20Travis%20Barker\).mp3' },
+      { title: 'Love Again \(Akinyele Back\) \(feat. Gangsta Boo\)', duration: '225', audioSrc: '/assets/music/RTJ2/09%20Love%20Again%20\(Akinyele%20Back\)%20\(feat.%20Gangsta%20Boo\).mp3' },
+      { title: 'Crown \(feat. Diane Coffee\)', duration: '226', audioSrc: '/assets/music/RTJ2/10%20Crown%20\(feat.%20Diane%20Coffee\).mp3' },
+      { title: 'Angel Duster', duration: '309', audioSrc: '/assets/music/RTJ2/11%20Angel%20Duster.mp3' },
+    ]
+  }, {
+    title: 'A Kid Named Cudi',
+    artist: 'Kid Cudi',
+    releaseInfo: '2008 10.Deep, Fool\'s Gold',
+    albumCover: '/assets/images/album_covers/aknk.jpg',
+    slug: 'a-kid-named-cudi',
+    songs: [
+      { title: 'Intro', duration: '50', audioSrc: '/assets/music/AKNK/01.Intro.mp3' },
+      { title: 'Down & Out', duration: '249', audioSrc: '/assets/music/AKNK/02.DownAndOut.mp3' },
+      { title: 'Is There Any Love Feat. Wale ', duration: '211', audioSrc: '/assets/music/AKNK/03.IsThereAnyLove.mp3' },
+      { title: 'CuDi Get', duration: '140', audioSrc: '/assets/music/AKNK/04.CuDiGet.mp3' },
+      { title: 'Man On The Moon \(The Anthem\)', duration: '207', audioSrc: '/assets/music/AKNK/05.ManOnTheMoon.mp3' },
+      { title: 'The Prayer', duration: '219', audioSrc: '/assets/music/AKNK/06.ThePrayer.mp3' },
+      { title: 'Day N Nite', duration: '168', audioSrc: '/assets/music/AKNK/07.DayNNite.mp3' },
+      { title: 'Embrace The Martian Feat. KiD CuDi', duration: '215', audioSrc: '/assets/music/AKNK/08.EmbraceTheMartian.mp3' },
+      { title: 'Maui Wowie', duration: '144', audioSrc: '/assets/music/AKNK/09.MauiWowie.mp3' },
+      { title: '50 Ways To Make A Record', duration: '175', audioSrc: '/assets/music/AKNK/10.50WaysToMakeARecord.mp3' },
+      { title: 'Whenever', duration: '124', audioSrc: '/assets/music/AKNK/11.Whenever.mp3' },
+      { title: 'Pillow Talk', duration: '204', audioSrc: '/assets/music/AKNK/12.PillowTalk.mp3' },
+      { title: 'Save My Soul \(The CuDi Confession\)', duration: '123', audioSrc: '/assets/music/AKNK/13.SaveMySoul.mp3' },
+      { title: 'TGIF Feat. Chip The Ripper', duration: '143', audioSrc: '/assets/music/AKNK/14.TGIF.mp3' },
+      { title: 'CuDi Spazzin', duration: '186', audioSrc: '/assets/music/AKNK/15.CuDiSpazzin.mp3' },
+      { title: 'Cleveland Is The Reason', duration: '226', audioSrc: '/assets/music/AKNK/16.ClevelandIsTheReason.mp3' },
+      { title: 'Heaven At Nite', duration: '197', audioSrc: '/assets/music/AKNK/17.HeavenAtNite.mp3' },
+    ]
   }
 ];
