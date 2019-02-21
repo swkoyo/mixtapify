47222a77f6daa0d84ce2deae88294412be25a1ec
Brandon Kim <bkim2490@gmail.com>
2019-02-21 22:01:19 +0900

    basic layout done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Route, Link } from 'react-router-dom';
-import { Container, Button, Input, Grid, Card, Image } from 'semantic-ui-react';
+import { Container, Button, Input, Grid, Card, Image, Header } from 'semantic-ui-react';
 import './landing.css';
 
 const Landing = () => (
@@ -34,34 +34,43 @@ const Landing = () => (
   //       </div>
   //     </div>
   //   </section>
-  //   <footer className="landing-footer">
-  //     <Link to='/'><img className="logo-photo" src="/assets/images/bloc_jams_logo.png" alt="Bloc Jams Logo"></img></Link>
-  //     <a href="#"><i className="fab fa-facebook-square"></i></a>
-  //     <a href="#"><i className="fab fa-twitter-square"></i></a>
-  //     <a href="#"><i className="fab fa-linkedin"></i></a>
-  //     <a href="#"><i className="fab fa-instagram"></i></a>
-  //   </footer>
+    // <footer className="landing-footer">
+    //   <Link to='/'><img className="logo-photo" src="/assets/images/bloc_jams_logo.png" alt="Bloc Jams Logo"></img></Link>
+    //   <a href="#"><i className="fab fa-facebook-square"></i></a>
+    //   <a href="#"><i className="fab fa-twitter-square"></i></a>
+    //   <a href="#"><i className="fab fa-linkedin"></i></a>
+    //   <a href="#"><i className="fab fa-instagram"></i></a>
+    // </footer>
   // </section>
   <Fragment>
-    <Grid columns={1}>
-      <Grid.Row>
-        <Grid.Column>
-          <h1>Welcome to Bloc Jams!</h1>
-          <h4>Sign-up for a new account</h4>
-          <form>
-            <Input className="email" type="email" placeholder="Enter email address" />
-            <Button>Click Here</Button>
-          </form>
-          <Link className="mainLibraryLink" to='/library'>Browse Our Selection!</Link>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-    <Container>
+    <Container fluid>
+      <Grid columns={1}>
+        <Grid.Row>
+          <Grid.Column>
+            <Header
+              size='huge'
+              content='Welcome to Mixtapify'
+              style={{
+                'font-size': '50px'
+              }}
+            />
+            {/* <Header as='h3'>Sign-up for a new account</Header>
+            <form>
+              <Input className="email" type="email" placeholder="Enter email address" />
+              <Button>Click Here</Button>
+            </form> */}
+            <Link className="mainLibraryLink" to='/library'>Browse Our Selection!</Link>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+    <Container style={{'padding-bottom': '100px'}}>
       <Grid columns={3} stretched>
+        <Header as='h3'>Mixtapify gives you instant access to millions of songs – from old favorites to the latest hits. Just hit play to stream anything you like.</Header>
         <Grid.Row>
           <Grid.Column>
             <Card fluid>
-              <Image src="/assets/images/selection.png" alt="Music Selection" />
+              <Image style={{'height': '300px'}} src="/assets/images/selection.png" alt="Music Selection" />
               <Card.Content>
                 <Card.Header>Choose your music</Card.Header>
                 <Card.Description>The world is full of music; why should you have to listen to music that someone else chose?</Card.Description>
@@ -70,7 +79,7 @@ const Landing = () => (
           </Grid.Column>
           <Grid.Column>
             <Card fluid>
-              <Image src="/assets/images/listening.png" alt="Music Streaming" />
+              <Image style={{'height': '300px'}} src="/assets/images/listening.png" alt="Music Streaming" />
               <Card.Content>
                 <Card.Header>Unlimited streaming, ad-free</Card.Header>
                 <Card.Description>No arbitrary limits. No distractions.</Card.Description>
@@ -79,7 +88,7 @@ const Landing = () => (
           </Grid.Column>
           <Grid.Column>
             <Card fluid>
-              <Image src="/assets/images/mobile.png" alt="Mobile Music" />
+              <Image style={{'height': '300px'}} src="/assets/images/mobile.png" alt="Mobile Music" />
               <Card.Content>
                 <Card.Header>Choose your music</Card.Header>
                 <Card.Description>Listen to your music on the go. This streaming service is avaliable on all mobile platforms.</Card.Description>
