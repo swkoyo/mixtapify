d6caaa04322db3c5ab18090596cf0dbedb8edf35
swkoyo <10441291+swkoyo@users.noreply.github.com>
2019-02-25 17:28:56 +0900

    acid rap added

--- src/data/albums.js
+++ src/data/albums.js
@@ -34,13 +34,13 @@ export default [{
       { title: 'Jeopardy', duration: '202', audioSrc: '/assets/music/RTJ2/01%20Jeopardy.mp3' },
       { title: 'Oh My Darling Don\'t Cry', duration: '205', audioSrc: '/assets/music/RTJ2/02%20Oh%20My%20Darling%20Don\'t%20Cry.mp3' },
       { title: 'Blockbuster Night Part 1', duration: '152', audioSrc: '/assets/music/RTJ2/03%20Blockbuster%20Night%20Part%201.mp3' },
-      { title: 'Close Your Eyes \(And Count To F***\) \(Feat. Zack De La Rocha\)', duration: '234', audioSrc: '/assets/music/RTJ2/04%20Close%20Your%20Eyes%20\(And%20Count%20To%20Fuck\)%20\(Feat.%20Zack%20De%20La%20Rocha\).mp3' },
+      { title: 'Close Your Eyes (And Count To F***) (Feat. Zack De La Rocha)', duration: '234', audioSrc: '/assets/music/RTJ2/04%20Close%20Your%20Eyes%20(And%20Count%20To%20Fuck)%20(Feat.%20Zack%20De%20La%20Rocha).mp3' },
       { title: 'All My Life', duration: '188', audioSrc: '/assets/music/RTJ2/05%20All%20My%20Life.mp3' },
       { title: 'Lie, Cheat, Steal', duration: '209', audioSrc: '/assets/music/RTJ2/06%20Lie,%20Cheat,%20Steal.mp3' },
-      { title: 'Early \(feat. BOOTS\)', duration: '224', audioSrc: '/assets/music/RTJ2/07%20Early%20\(feat.%20BOOTS\).mp3' },
-      { title: 'All Due Respect', duration: '168', audioSrc: '/assets/music/RTJ2/08%20All%20Due%20Respect%20\(feat.%20Travis%20Barker\).mp3' },
-      { title: 'Love Again \(Akinyele Back\) \(feat. Gangsta Boo\)', duration: '225', audioSrc: '/assets/music/RTJ2/09%20Love%20Again%20\(Akinyele%20Back\)%20\(feat.%20Gangsta%20Boo\).mp3' },
-      { title: 'Crown \(feat. Diane Coffee\)', duration: '226', audioSrc: '/assets/music/RTJ2/10%20Crown%20\(feat.%20Diane%20Coffee\).mp3' },
+      { title: 'Early (feat. BOOTS)', duration: '224', audioSrc: '/assets/music/RTJ2/07%20Early%20(feat.%20BOOTS).mp3' },
+      { title: 'All Due Respect', duration: '168', audioSrc: '/assets/music/RTJ2/08%20All%20Due%20Respect%20(feat.%20Travis%20Barker).mp3' },
+      { title: 'Love Again (Akinyele Back) (feat. Gangsta Boo)', duration: '225', audioSrc: '/assets/music/RTJ2/09%20Love%20Again%20(Akinyele%20Back)%20(feat.%20Gangsta%20Boo).mp3' },
+      { title: 'Crown (feat. Diane Coffee)', duration: '226', audioSrc: '/assets/music/RTJ2/10%20Crown%20(feat.%20Diane%20Coffee).mp3' },
       { title: 'Angel Duster', duration: '309', audioSrc: '/assets/music/RTJ2/11%20Angel%20Duster.mp3' },
     ]
   }, {
@@ -54,7 +54,7 @@ export default [{
       { title: 'Down & Out', duration: '249', audioSrc: '/assets/music/AKNK/02.DownAndOut.mp3' },
       { title: 'Is There Any Love Feat. Wale ', duration: '211', audioSrc: '/assets/music/AKNK/03.IsThereAnyLove.mp3' },
       { title: 'CuDi Get', duration: '140', audioSrc: '/assets/music/AKNK/04.CuDiGet.mp3' },
-      { title: 'Man On The Moon \(The Anthem\)', duration: '207', audioSrc: '/assets/music/AKNK/05.ManOnTheMoon.mp3' },
+      { title: 'Man On The Moon (The Anthem)', duration: '207', audioSrc: '/assets/music/AKNK/05.ManOnTheMoon.mp3' },
       { title: 'The Prayer', duration: '219', audioSrc: '/assets/music/AKNK/06.ThePrayer.mp3' },
       { title: 'Day N Nite', duration: '168', audioSrc: '/assets/music/AKNK/07.DayNNite.mp3' },
       { title: 'Embrace The Martian Feat. KiD CuDi', duration: '215', audioSrc: '/assets/music/AKNK/08.EmbraceTheMartian.mp3' },
@@ -62,7 +62,7 @@ export default [{
       { title: '50 Ways To Make A Record', duration: '175', audioSrc: '/assets/music/AKNK/10.50WaysToMakeARecord.mp3' },
       { title: 'Whenever', duration: '124', audioSrc: '/assets/music/AKNK/11.Whenever.mp3' },
       { title: 'Pillow Talk', duration: '204', audioSrc: '/assets/music/AKNK/12.PillowTalk.mp3' },
-      { title: 'Save My Soul \(The CuDi Confession\)', duration: '123', audioSrc: '/assets/music/AKNK/13.SaveMySoul.mp3' },
+      { title: 'Save My Soul (The CuDi Confession)', duration: '123', audioSrc: '/assets/music/AKNK/13.SaveMySoul.mp3' },
       { title: 'TGIF Feat. Chip The Ripper', duration: '143', audioSrc: '/assets/music/AKNK/14.TGIF.mp3' },
       { title: 'CuDi Spazzin', duration: '186', audioSrc: '/assets/music/AKNK/15.CuDiSpazzin.mp3' },
       { title: 'Cleveland Is The Reason', duration: '226', audioSrc: '/assets/music/AKNK/16.ClevelandIsTheReason.mp3' },
@@ -75,7 +75,19 @@ export default [{
     albumCover: '/assets/images/album_covers/acidrap.jpg',
     slug: 'acid-rap',
     songs: [
-      { title: 'Good Ass Intro'}
+      { title: 'Good Ass Intro', duration: '239', audioSrc: '/assets/music/acidrap/01.GoodAssIntro.mp3' },
+      { title: 'Pusha Man', duration: '444', audioSrc: '/assets/music/acidrap/02.PushaMan.mp3' },
+      { title: 'Cocoa Butter Kisses', duration: '307', audioSrc: '/assets/music/acidrap/03.CocoaButterKisses.mp3' },
+      { title: 'Juice', duration: '215', audioSrc: '/assets/music/acidrap/04.Juice.mp3' },
+      { title: 'Lost', duration: '185', audioSrc: '/assets/music/acidrap/05.Lost.mp3' },
+      { title: 'Everybody\'s Something', duration: '277', audioSrc: '/assets/music/acidrap/06.EverybodysSomething.mp3' },
+      { title: 'Interlude - That\'s Love', duration: '150', audioSrc: '/assets/music/acidrap/07.Interlude-ThatsLove.mp3' },
+      { title: 'Favorite Song', duration: '185', audioSrc: '/assets/music/acidrap/08.FavoriteSong.mp3' },
+      { title: 'NaNa', duration: '201', audioSrc: '/assets/music/acidrap/09.NaNa.mp3' },
+      { title: 'Smoke Again', duration: '273', audioSrc: '/assets/music/acidrap/10.SmokeAgain.mp3' },
+      { title: 'Acid Rain', duration: '216', audioSrc: '/assets/music/acidrap/11.AcidRain.mp3' },
+      { title: 'Chain Smoker', duration: '211', audioSrc: '/assets/music/acidrap/12.ChainSmoker.mp3' },
+      { title: 'Everything\'s Good (Good Ass Outro)', duration: '333', audioSrc: '/assets/music/acidrap/13.EverythingsGood.mp3' }
     ]
   }
 ];
