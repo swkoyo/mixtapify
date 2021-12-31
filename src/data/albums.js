4dd1e1512ff84d2571a90efd1865724e71fcc4c1
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-31 17:06:25 +0900

    clean up

--- src/data/albums.js
+++ src/data/albums.js
@@ -25,16 +25,17 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
-    title: 'Run the Jewels 2',
-    artist: 'Run the Jewels',
-    releaseInfo: '2014 Mass Appeal',
-    albumCover: '/assets/images/album_covers/rtj2.png',
-    slug: 'run-the-jewels-2',
+    title: 'The Summer',
+    artist: 'Tyler Durden',
+    releaseInfo: 'Paper Street Records',
+    albumCover: '/assets/images/album_covers/03.jpg',
+    slug: 'the-telephone',
     songs: [
-      { title: 'Jeopardy', duration: '202', audioSrc: '/assets/music/RTJ2/01%20Jeopardy.mp3' },
-      { title: 'Oh My Darling Don\'t Cry', duration: '205', audioSrc: '/assets/music/RTJ2/02%20Oh%20My%20Darling%20Don\'t%20Cry.mp3' },
-      { title: 'Blockbuster Night Part 1', duration: '152', audioSrc: '/assets/music/RTJ2/03%20Blockbuster%20Night%20Part%201.mp3' },
-      { title: 'Close Your Eyes (And Count To F***) (Feat. Zack De La Rocha)', duration: '234', audioSrc: '/assets/music/RTJ2/04%20Close%20Your%20Eyes%20(And%20Count%20To%20Fuck)%20(Feat.%20Zack%20De%20La%20Rocha).mp3' }
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
     title: 'A Kid Named Cudi',
@@ -43,10 +44,11 @@ export default [{
     albumCover: '/assets/images/album_covers/aknk.jpg',
     slug: 'a-kid-named-cudi',
     songs: [
-      { title: 'Intro', duration: '50', audioSrc: '/assets/music/AKNK/01.Intro.mp3' },
-      { title: 'Down & Out', duration: '249', audioSrc: '/assets/music/AKNK/02.DownAndOut.mp3' },
-      { title: 'Is There Any Love Feat. Wale ', duration: '211', audioSrc: '/assets/music/AKNK/03.IsThereAnyLove.mp3' },
-      { title: 'CuDi Get', duration: '140', audioSrc: '/assets/music/AKNK/04.CuDiGet.mp3' }
+		{ title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+		{ title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+		{ title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+		{ title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+		{ title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
     title: 'Acid Rap',
@@ -55,10 +57,11 @@ export default [{
     albumCover: '/assets/images/album_covers/acidrap.jpg',
     slug: 'acid-rap',
     songs: [
-      { title: 'Good Ass Intro', duration: '239', audioSrc: '/assets/music/acidrap/01.GoodAssIntro.mp3' },
-      { title: 'Pusha Man', duration: '444', audioSrc: '/assets/music/acidrap/02.PushaMan.mp3' },
-      { title: 'Cocoa Butter Kisses', duration: '307', audioSrc: '/assets/music/acidrap/03.CocoaButterKisses.mp3' },
-      { title: 'Juice', duration: '215', audioSrc: '/assets/music/acidrap/04.Juice.mp3' }
+		{ title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+		{ title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+		{ title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+		{ title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+		{ title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
     title: '1999',
@@ -67,10 +70,11 @@ export default [{
     albumCover: '/assets/images/album_covers/1999.jpg',
     slug: '1999',
     songs: [
-      { title: 'Summer Knights', duration: '116', audioSrc: '/assets/music/1999/01.SummerKnights.mp3' },
-      { title: 'Waves', duration: '212', audioSrc: '/assets/music/1999/02.Waves.mp3' },
-      { title: 'FromdaTomb Feat. Chunk Strangers', duration: '205', audioSrc: '/assets/music/1999/03.FromdaTomb.mp3' },
-      { title: 'Survival Tactics Feat. Capital STEEZ', duration: '203', audioSrc: '/assets/music/1999/04.SurvivalTactics.mp3' }
+		{ title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+		{ title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+		{ title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+		{ title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+		{ title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
     title: 'LiveLoveASAP',
@@ -79,10 +83,11 @@ export default [{
     albumCover: '/assets/images/album_covers/liveloveasap.jpg',
     slug: 'liveloveasap',
     songs: [
-      { title: 'Palace', duration: '166', audioSrc: '/assets/music/liveloveasap/01.Palace.mp3' },
-      { title: 'Peso', duration: '171', audioSrc: '/assets/music/liveloveasap/02.Peso.mp3' },
-      { title: 'Bass', duration: '201', audioSrc: '/assets/music/liveloveasap/03.Bass.mp3' },
-      { title: 'Wassup', duration: '161', audioSrc: '/assets/music/liveloveasap/04.Wassup.mp3' }
+		{ title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+		{ title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+		{ title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+		{ title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+		{ title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
     title: 'BetterOffDEAD',
@@ -91,10 +96,11 @@ export default [{
     albumCover: '/assets/images/album_covers/betteroffdead.jpg',
     slug: 'betteroffdead',
     songs: [
-      { title: 'Amerikan Pie', duration: '269', audioSrc: '/assets/music/betteroffdead/01.AmerikkkanPie.mp3' },
-      { title: 'Nephilim', duration: '200', audioSrc: '/assets/music/betteroffdead/02.Nephilim.mp3' },
-      { title: 'Bliss', duration: '204', audioSrc: '/assets/music/betteroffdead/03.Bliss.mp3' },
-      { title: 'Mine', duration: '118', audioSrc: '/assets/music/betteroffdead/04.Minephuck.mp3' }
+		{ title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+		{ title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+		{ title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+		{ title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+		{ title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }
 ];
